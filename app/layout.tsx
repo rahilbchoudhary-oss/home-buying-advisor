82c089cb517dd512685aae02b964556c7626e9c0
rahilbchoudhary-oss <rahilbchoudhary@gmail.com>
2026-08-31 17:41:12 +0530

    Integrate Cuelinks affiliate tracking script
    Added Cuelinks affiliate tracking script to layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Link from "next/link";
+import Script from "next/script";
 
 export const metadata = {
   title: "Home Buying Advisor",
@@ -56,6 +57,21 @@ export default function RootLayout({
         </header>
 
         {children}
+
+        {/* Cuelinks affiliate tracking */}
+        <Script id="cuelinks-script">
+          {`
+            var cId = '315432';
+
+            (function(d, t) {
+              var s = document.createElement('script');
+              s.type = 'text/javascript';
+              s.async = true;
+              s.src = (document.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/') + 'cuelinksv2.js';
+              document.getElementsByTagName('body')[0].appendChild(s);
+            }());
+          `}
+        </Script>
       </body>
     </html>
   );
