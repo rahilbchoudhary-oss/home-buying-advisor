0e248eb93003c66cc629c9436c18016d65fa0c49
rahilbchoudhary-oss <rahilbchoudhary@gmail.com>
2026-08-31 17:57:42 +0530

    Fix script tag formatting for Cuelinks tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,19 +59,17 @@ export default function RootLayout({
         {children}
 
         {/* Cuelinks affiliate tracking */}
-        <Script id="cuelinks-script">
-          {`
-            var cId = '315432';
+        <script type="text/javascript">
+    var cId =  "315432";
 
-            (function(d, t) {
-              var s = document.createElement('script');
-              s.type = 'text/javascript';
-              s.async = true;
-              s.src = (document.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/') + 'cuelinksv2.js';
-              document.getElementsByTagName('body')[0].appendChild(s);
-            }());
-          `}
-        </Script>
+    (function(d, t) {
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = (document.location.protocol == "https:" ? "https://cdn0.cuelinks.com/js/" : "http://cdn0.cuelinks.com/js/")  + "cuelinksv2.js";
+      document.getElementsByTagName("body")[0].appendChild(s);
+    }());
+</script>
       </body>
     </html>
   );
