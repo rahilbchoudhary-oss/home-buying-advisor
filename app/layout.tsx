77313baa6ef1dc9728afbb9479f914f6685f2b5c
rahilbchoudhary-oss <rahilbchoudhary@gmail.com>
2026-08-31 22:10:59 +0530

    Update Cuelinks tracking script implementation
    Replaced Script component with a script tag for Cuelinks tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import Link from "next/link";
-import Script from "next/script";
 
 export const metadata = {
   title: "Home Buying Advisor",
@@ -58,22 +57,22 @@ export default function RootLayout({
 
         {children}
 
-        {/* Cuelinks affiliate tracking */}
+        {/* Cuelinks JavaScript */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var cId = '315432';
 
-        <Script id="cuelinks-script">
-          {`
-            var cId = '315432';
-
-            (function(d, t) {
-              var s = document.createElement('script');
-              s.type = 'text/javascript';
-              s.async = true;
-              s.src = (document.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/') + 'cuelinksv2.js';
-              document.getElementsByTagName('body')[0].appendChild(s);
-            }());
-          `}
-        </Script>
-        
+              (function(d, t) {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = (document.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/') + 'cuelinksv2.js';
+                document.getElementsByTagName('body')[0].appendChild(s);
+              }());
+            `,
+          }}
+        />
       </body>
     </html>
   );
